216eed9afacddc0d515284d94f854b58f5887cd9
Marcel Suter <marcel.suter@bzz.ch>
2024-07-25 13:08:44 +0200

    added files for new autograding

--- door_test.py
+++ door_test.py
@@ -1,34 +1,30 @@
 import pytest
+
 from door import Door, DoorLock
 
+@pytest.fixture
+def a_door():
+    return Door(DoorLock(), 'green')
 
-class TestDoor:
+def test_initial(a_door):
+    assert a_door.color == 'green'
 
-    @pytest.fixture
-    def a_door(self):
-        return Door(DoorLock(), 'green')
+def test_get_set_color(a_door):
+    a_door.color = 'red'
+    assert a_door.color == 'red'
 
+def test_door_open(a_door):
+    a_door.open_the_door()
+    assert a_door.door_is_open
 
-    def test_initial(self,a_door):
-        assert a_door.color == 'green'
+def test_door_close(a_door):
+    a_door.close_the_door()
+    assert a_door.door_is_open == False
 
-    def test_get_set_color(self, a_door):
-        a_door.color = 'red'
-        assert a_door.color == 'red'
+def test_door_locking(a_door):
+    a_door.lock_the_door()
+    assert a_door.door_ist_locked
 
-
-    def test_door_open(self, a_door):
-        a_door.open_the_door()
-        assert a_door.door_is_open
-    def test_door_close(self, a_door):
-        a_door.close_the_door()
-        assert a_door.door_is_open == False
-
-    def test_door_locking(self, a_door):
-        a_door.lock_the_door()
-        assert a_door.door_ist_locked
-
-    def test_door_unlock(self, a_door):
-        a_door.unlock_the_door()
-        assert a_door.door_ist_locked == False
-
+def test_door_unlock(a_door):
+    a_door.unlock_the_door()
+    assert a_door.door_ist_locked == False
